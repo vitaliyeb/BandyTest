0657c4101bbbb12f1439914fc2bbd6a49088482b
Vitaliy_B <vlvlv.vovov@bk.ru>
2020-04-01 19:20:59 +0500

    логика формы создания комментария

--- src/components/add-comment/add-comment.js
+++ src/components/add-comment/add-comment.js
@@ -2,24 +2,47 @@ import React, {Component} from 'react';
 import './add-comment.styl'
 
 class AddComment extends Component{
+    constructor(){
+        super();
+        this.state = {
+            name: '',
+            email: '',
+            comment: ''
+        };
+        this.addComment = (e) => {
+            e.preventDefault();
+            if (!this.state.email.replace(/\s+/g,'').length) return null;
+        };
+
+    }
+
+    setInput(e, property){
+        let state = this.state,
+            value = e.target.value;
+        this.setState({...state, [property]: value})
+    }
 
     render() {
+        let {name, email, comment} = this.state;
         return(
-            <div className='add-comment'>
+            <form className='add-comment' onSubmit={this.addComment}>
                 <p className='add-comment__heading'>Добавить комментарий</p>
                 <div className='add-comment__wrapper'>
                     <p className='add-comment__title'>Имя</p>
-                    <input className='add-comment__input' type="text"/>
+                    <input className='add-comment__input' onChange={(e)=>this.setInput(e, 'name')} value={name} type="text"/>
                 </div>
                 <div className='add-comment__wrapper'>
                     <p className='add-comment__title'>E-mail</p>
-                    <input className='add-comment__input' type="text"/>
+                    <input className='add-comment__input' onChange={(e)=>this.setInput(e, 'email')} value={email} type="email"/>
                 </div>
                 <div className='add-comment__wrapper'>
                     <p className='add-comment__title'>Текст комментария</p>
-                    <textarea className='add-comment__input add-comment__input_comment'/>
+                    <div className='add-comment__wrapper_button'>
+                        <textarea className='add-comment__input add-comment__input_comment' onChange={(e)=>this.setInput(e, 'comment')} value={comment}/>
+                        <input className='add-comment__submit' type="submit" value='Добавить коментарий'/>
+                    </div>
                 </div>
-            </div>
+            </form>
         )
     }
 }
